eecf1ff9e662fefe1bbcf915672f94e63675365a
gyu <leesk000107@naver.com>
2026-09-09 12:43:53 +0900

    fix : RDS 용 주석 삭제

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -53,7 +53,7 @@ resource "aws_subnet" "private" {
   )
 }
 
-# 5. 데이터베이스 서브넷 (추후 RDS용)
+# 5. 데이터베이스 서브넷
 resource "aws_subnet" "database" {
   count             = length(var.database_subnet_cidrs)
   vpc_id            = aws_vpc.main.id
