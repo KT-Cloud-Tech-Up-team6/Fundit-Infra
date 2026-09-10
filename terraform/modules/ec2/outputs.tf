9ab563da1b5a1385ad52d88c5301cb0df23e945d
gyu <leesk000107@naver.com>
2026-09-10 16:05:02 +0900

    fix : CDN App 입력 도메인 변경

--- terraform/modules/ec2/outputs.tf
+++ terraform/modules/ec2/outputs.tf
@@ -7,3 +7,8 @@ output "public_ip" {
   description = "EC2에 할당된 고정 공인 IP (EIP)"
   value       = aws_eip.app.public_ip
 }
+
+output "public_dns" {
+  description = "EC2 퍼블릭 DNS 도메인"
+  value       = aws_instance.app.public_dns
+}
