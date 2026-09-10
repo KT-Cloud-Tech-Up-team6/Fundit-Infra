cb40646d66d655f2e5df53f0cd261d6d38df5a6b
mshjgr <mshjgr@gmail.com>
2026-09-10 12:54:33 +0900

    fix: 하드코딩된 AWS profile "final"을 "team6-infra"로 수정

--- terraform/envs/bootstrap/backend.tf
+++ terraform/envs/bootstrap/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket       = "fundit-tfstate-team6"
     key          = "bootstrap/terraform.tfstate" # bootstrap 전용 경로
     region       = "ap-northeast-2"
-    profile      = "final"
+    profile      = "team6-infra"
     use_lockfile = true # S3 Native Lock
   }
 }
